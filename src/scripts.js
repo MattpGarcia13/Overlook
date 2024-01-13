2d942b884e9aee58e7d0d5451bbd9b7daf1ed777
Matt Garcia <prebccentral@gmail.com>
2024-01-13 16:56:55 -0700

    finally fixed background image and initial CSS

--- src/scripts.js
+++ src/scripts.js
@@ -9,3 +9,25 @@ import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+})
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ]
     },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: [
           {
             loader: 'file-loader',
