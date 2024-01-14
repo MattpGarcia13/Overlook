ffaaa48249607231ee8ecf4de399371d4ba2e0cc
Matt Garcia <prebccentral@gmail.com>
2024-01-14 15:43:30 -0700

    Created functionality for user to see bookings

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,6 +1,7 @@
 const getPromises = [
     fetch('http://localhost:3001/api/v1/customers/'),
-    fetch('http://localhost:3001/api/v1/bookings')
+    fetch('http://localhost:3001/api/v1/bookings'),
+    fetch('http://localhost:3001/api/v1/rooms')
 ];
 export const fetchData = () => {
     return Promise.all(getPromises);
--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import { fetchData } from './api-calls';
+import { showPastTrips, showUpcomingTrips } from './domUpdates';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
@@ -17,12 +18,30 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-var holdCustomer
-var bookings
+
+const planTripsBtn = document.querySelector('#planTrips');
+const pastTripsBtn = document.querySelector('#pastTrips');
+const upcomingTripsBtn = document.querySelector('#upcomingTrips');
+const planTripSection = document.querySelector('#planTripSection');
+const pastTripSection = document.querySelector('#pastTripSection');
+const upcomingTripSection = document.querySelector('#upcomingTripSection');
+const loginButton = document.querySelector('#loginButton');
+
+let allCustomers
+let bookings
+let rooms
+let currentCustomerID
 
 window.addEventListener('load', () => {
     retrieveData()
 })
+
+const loginUser = () => {
+    //WIll need to do user authentification HERE-------
+    currentCustomerID = 50
+    showPastTrips(currentCustomerID, bookings)
+    showUpcomingTrips(currentCustomerID, bookings)
+}
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -54,8 +73,9 @@ function retrieveData() {
         })
         .then(data => {
             console.log(data);
-            holdCustomer = data[0].customers[5];
+            allCustomers = data[0].customers;
             bookings = data[1].bookings;
+            rooms = data[2].rooms;
         })
         .catch(err => console.log(err.message, err));
 }
@@ -65,9 +85,41 @@ tabs.forEach(tab => {
         const target = document.querySelector(`#${tab.id}Content`)
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
-            console.log(tab.id)
         })
         target.classList.add('active')
     })
+})
+
+const toggleBookingsSection = (tabName) => {
+    if (tabName === 'planTrips') {
+        planTripSection.hidden = false;
+        pastTripSection.hidden = true;
+        upcomingTripSection.hidden = true;
+    } else if (tabName === 'pastTrips') {
+        planTripSection.hidden = true;
+        pastTripSection.hidden = false;
+        upcomingTripSection.hidden = true;
+    } else if (tabName === 'upcomingTrips') {
+        planTripSection.hidden = true;
+        pastTripSection.hidden = true;
+        upcomingTripSection.hidden = false;
+    }
+}
+
+
+planTripsBtn.addEventListener('click', () => {
+    toggleBookingsSection('planTrips')
+})
+
+pastTripsBtn.addEventListener('click', () => {
+    toggleBookingsSection('pastTrips')
+})
+
+upcomingTripsBtn.addEventListener('click', () => {
+    toggleBookingsSection('upcomingTrips')
+})
+
+loginButton.addEventListener('click', () => {
+    loginUser()
 })
 
